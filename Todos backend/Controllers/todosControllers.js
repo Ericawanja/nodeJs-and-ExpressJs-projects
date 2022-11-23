8efd32f261df4c5b4f10bed7b8a2fa1b0fea10ad
Ericawanja <wanjaerica@gmail.com>
2022-11-23 15:45:26 +0300

    delete and update

--- Todos backend/Controllers/todosControllers.js
+++ Todos backend/Controllers/todosControllers.js
@@ -25,25 +25,77 @@ const getOneTodo = async (req, res) => {
       .input("todo_id", todo_id)
       .execute("getOneTodo");
     const todo = await response.recordset;
-    console.log(todo);
-    res.status(200).json(todo);
+
+    if (todo.length > 0) {
+      res.status(200).json(todo);
+    } else {
+      res.status(404).json({ message: `Todo with id ${id} does not exist` });
+    }
   } catch {}
 };
 
 //endpoint to insert a todo
 
 const insertTodo = async (req, res) => {
-  const todo_id = 'new';
+  const todo_id = v4();
   const { title, description, date } = req.body;
   const pool = await mssql.connect(sqlConfig);
   const response = await pool
     .request()
     .input("todo_id", todo_id)
-    .input('title', title)
+    .input("title", title)
     .input("description", description)
     .input("date", date)
-    .execute('insertTodo');
-  const todo = await response.recordset
-  res.status(201).json(todo)  
+    .execute("insertTodo");
+  const todo = await response.recordset;
+  res.status(201).json({ message: "you have succesfully added a todo" });
 };
-module.exports = { getTodos, getOneTodo, insertTodo };
+
+const updateTodo = async (req, res) => {
+  const { todo_id } = req.params;
+  const { title, description, date } = req.body;
+  try {
+    const pool = await mssql.connect(sqlConfig);
+    //check if the item exist first'
+    const response = await pool
+      .request()
+      .input("todo_id", todo_id)
+      .execute("getOneTodo");
+    const todo = await response.recordset;
+
+    const todo_in_db = todo.find((t) => t.todo_id === todo_id);
+    if (todo) {
+      //logic to update since it exists
+    } else {
+      res.status(200).json({ message: "the item does not exist in the db" });
+    }
+    await pool.execute
+      .input("todo_id", todo_id)
+      .input("title", title)
+      .input("description", description)
+      .input("date", date)
+      .execute("updateTodo");
+    res.status(200).json({ message: `You have succesfully updated the item` });
+  } catch {}
+};
+
+//delete todo
+
+const deleteTodo = async (req,res)=>{
+  const {todo_id} = req.params
+  const pool = await mssql.connect(sqlConfig)
+
+  //check if the item exists first
+  const todo = await (await pool.request().input('todo_id', todo_id).execute('getOneTodo')).recordset
+
+  if(todo.length > 0){
+    await pool.request().input('todo_id', todo_id).execute('deleteTodo')
+    res.status(200).json({"messege": `Succesful delete`})
+
+  }else{
+
+    res.status(404).json({"messege": `A todo with id ${todo_id} does not exist`})
+  }
+  
+}
+module.exports = { getTodos, getOneTodo, insertTodo, updateTodo, deleteTodo };
--- Todos backend/Routes/todosRoutes.js
+++ Todos backend/Routes/todosRoutes.js
@@ -2,11 +2,13 @@ const express = require('express')
 const todoRoutes = express.Router()
 
 //controllers
-const {getTodos, getOneTodo, insertTodo} = require('../Controllers/todosControllers')
+const {getTodos, getOneTodo, insertTodo, updateTodo, deleteTodo} = require('../Controllers/todosControllers')
 
 todoRoutes.get('/',getTodos)
 todoRoutes.get ('/:todo_id', getOneTodo)
 todoRoutes.post('/', insertTodo)
+todoRoutes.put('/:todo_id', updateTodo)
+todoRoutes.delete("/:todo_id", deleteTodo)
 
 
 module.exports= todoRoutes;
